3ab9fe134004c06a93629cd15f735362673fd73a
Sara-Kuddah <sara-kuddah@hotmail.com>
2020-03-08 11:16:07 +0300

    Fix routes index path

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose= require('mongoose');
 
 //Require Route Files
-const indexRouter =require('./routes/index');
+const indexRouter =require('./app/routes/index');
 
 // Instantite Express App Obj
 const app= express();
